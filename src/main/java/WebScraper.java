f9eff10e1a948bf6203325265cbf521a111d3cf7
Collins Kariuki <collinsmunkary@gmail.com>
2023-09-27 07:08:41 +0300

    A working Web Scrapper in Java

---
+++ src/main/java/WebScraper.java
@@ -0,0 +1,27 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+public class WebScraper {
+    public static void main(String[] args) {
+        String url = "https://www.scrapethissite.com/pages/simple/";
+        try {
+            Document doc = Jsoup.connect(url).get();
+//            System.out.println(doc);
+
+            Elements countries = doc.select(".country");
+
+            for (Element country : countries) {
+                String countryName = country.select(".country-name").text();
+                String capitalCity = country.select(".country-capital").text();
+                System.out.println(countryName + " - " + capitalCity);
+            }
+
+        } catch (IOException e) {
+            System.out.println("An IOException occurred. Please try again.");
+        }
+    }
+}
